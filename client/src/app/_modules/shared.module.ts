fe97f41e014d3a76209590ba6b8cf118bd9dbe38
Omar-Salem12 <omarsalem20200@gmail.com>
2021-05-31 03:33:39 +0200

    Add some frontend features

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { HttpClientModule} from '@angular/common/http';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
+
 
 
 
@@ -13,13 +18,25 @@ import { ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot({
       
       positionClass: 'toast-bottom-right',
-    })
+    }),
+    TabsModule.forRoot(),
+  
+    
+ 
+ 
+    
   ],
   
 exports: [
   BsDropdownModule,
  ToastrModule,
+ TabsModule,
+ 
+
+
+
 ]
   
 })
+
 export class SharedModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { TestErrorsComponent } from './Errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './Errors/_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 
 
@@ -37,6 +39,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
      TestErrorsComponent,
      NotFoundComponent,
      ServerErrorComponent,
+     MemberCardComponent,
      
    
   ],
@@ -54,7 +57,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 
   providers: [
 
-    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor,multi: true}
+    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor,multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor,multi: true}
   ],
   bootstrap: [AppComponent]
 })
